f1e8bc5e2f14f1cd92a4d4a4f5b7ae77dca4c78d
Joaquim <Joaquim@192.168.150.135>
2018-12-06 17:03:57 +0100

    Pas mal

--- wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
+++ wikimatrix/src/main/java/fr/univrennes1/istic/wikipediamatrix/App.java
@@ -1,13 +1,42 @@
 package fr.univrennes1.istic.wikipediamatrix;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args ) throws IOException{
+		System.out.println( "Hello World!" );
+
+//		/* test */
+//		String url ="https://fr.wikipedia.org/wiki/Liste_des_Pok%C3%A9mon_de_Pok%C3%A9mon_Rouge_et_Bleu";
+//		//String url ="https://en.wikipedia.org/wiki/Comparison_of_Canon_EOS_digital_cameras";
+//		Document doc = Jsoup.connect(url).get();
+//		System.out.println(doc.title());
+//		Elements tableau = doc.select("table");
+//		Table test = new Table("Titre",tableau.get(0));
+//		String str = test.toCSV(";");
+//		System.out.println(str);
+//		test.Test();
+//		System.out.println(test.observations.get(test.observations.size()-1).observation.toString());
+//		System.out.println(test.nom_table);
+//		
+//		Elements titre = doc.select("table caption");
+//		System.out.println(titre.text());
+//		
+//		
+		
+		String url = "https://www.pokepedia.fr/Liste_des_Pok%C3%A9mon_dans_l%27ordre_du_Pok%C3%A9dex_National";
+		Pageweb test=new Pageweb(url);
+		test.toCSV(";");
+		
+		
+	}
 }
